7c7a6627b0049b7c9eb541842da326abf2eb4e3a
ale <alias@axew3.com>
2018-05-26 02:07:19 +0200

    Update w3-ask.php

--- w3-ask.php
+++ w3-ask.php
@@ -2,14 +2,13 @@
 // manage/process and store streams
 foreach($_POST as $k => $p){
  if($k=='base64data'){
- 	$w3stream = substr(trim($p), 22);
- 	$w3stream = base64_decode($w3stream);
+ 	$w3stream = base64_decode(substr(trim($p), 22));
  } else {
 	 $fname = $p;
   }
 }
 if(!file_put_contents( "./".$fname, $w3stream )){
- echo"w3FWerror - the file not createt"; // ERROR: the file has not been created
+ echo"w3FWerror - file waw not created"; // ERROR: the file has not been created
 	exit;
 }
 
